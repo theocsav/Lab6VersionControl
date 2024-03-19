2211ab962d6bb6f69aa08f5f8d7cbe5ef6522a97
johanfortus <fortusjohan@gmail.com>
2024-03-19 15:35:33 -0400

    Added File

---
+++ version_control.py
@@ -0,0 +1,15 @@
+"""
+File: version_control.py
+Author: Johan Fortus
+Date: 3/19/24
+Description: Lab 06 - Version Control
+"""
+
+def main():
+    pass
+
+def encode(password):
+    pass
+
+if __name__ == "__main__":
+    main()
